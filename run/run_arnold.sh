9ede2ed30b9fc3b0662d42412ce179ed56b43527
sunzewei.v <sunzewei.v@bytedance.com>
2021-01-28 18:38:35 +0800

    modify scripts

--- run/run_arnold.sh
+++ run/run_arnold.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-echo "[logging] args: $@"
+export http_proxy=http://bj-rd-proxy.byted.org:3128
+export https_proxy=http://bj-rd-proxy.byted.org:3128
+
+export NCCL_IB_DISABLE=0
+export NCCL_IB_HCA=$ARNOLD_RDMA_DEVICE:1
+export NCCL_IB_GID_INDEX=3
+export NCCL_SOCKET_IFNAME=eth0
+
+cd $(dirname $0)
+sudo pip install --editable ../ -i https://pypi.doubanio.com/simple
 
 cd /opt/tiger/fairseq/run
 echo "current dir: `pwd`"
+echo "[logging] args: $@"
 
 hadoop fs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/sunzewei.v/pretrain/scripts/$1
 
