c13a8d078874701bc470b551ec33d739ff0cc155
sunzewei.v <sunzewei.v@bytedance.com>
2021-01-28 15:49:12 +0800

    fix the dir

--- run/run_arnold.sh
+++ run/run_arnold.sh
@@ -2,7 +2,7 @@
 
 echo "[logging] args: $@"
 
-cd fairseq/run
+cd /opt/tiger/fairseq/run
 echo "current dir: `pwd`"
 
 hadoop fs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/sunzewei.v/pretrain/scripts/$1
